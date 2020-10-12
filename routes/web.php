c20cfcc5a48950c73dfd427aff5cad02e1bf1b38
nangirvan <nangirvan98@gmail.com>
2020-10-12 13:12:44 +0700

    Defining the routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+
+Route::post('/add-activity', 'ActivityController@addActivity');
+Route::post('/add-progress', 'ProgressController@addProgress');
+
+Route::get('/update-progress', 'ProgressController@updateProgress');
